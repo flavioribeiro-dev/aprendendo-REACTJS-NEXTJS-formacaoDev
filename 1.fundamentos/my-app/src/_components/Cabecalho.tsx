57bcb1a4f33bec2e51958d83dfb5e8ab349ad729
fribeiro <flavioxconsult@gmail.com>
2025-12-04 22:59:10 -0300

    criado um primeiro Componente: "Cabecalho"

---
+++ 1.fundamentos/my-app/src/_components/Cabecalho.tsx
@@ -0,0 +1,10 @@
+export default function Cabecalho() {
+    return (
+        <div className={`
+            flex justify-center items-center
+            bg-purple-500 h-36 text-3xl rounded-lg
+        `}>
+            Cabeçalho
+        </div>
+    )
+}
---
+++ 1.fundamentos/my-app/src/app/fundamentos/pagina/page.tsx
@@ -0,0 +1,12 @@
+import Cabecalho from "@/_components/Cabecalho";
+
+export default function Page() {
+    return (
+        <div className={`
+            p-4
+        `}>
+            <Cabecalho />
+            Montar uma página
+        </div>
+    )
+}
